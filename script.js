67316437d2b3a9915c99b8947678613d32dba2a0
Assefa Tadesse <assekopsynwa@gmail.com>
2023-10-07 00:48:42 -0500

    Button Animate

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
+function openTab(th)
+            {
+                window.open(th.name,'_blank');
+            }
+            
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
+}
+
+var header=document.getElementById("header")
+var navbar=document.getElementById("navbar")
+
+window.onscroll=function(){
+    if(window.scrollY>=menu.offsetTop)
 }
